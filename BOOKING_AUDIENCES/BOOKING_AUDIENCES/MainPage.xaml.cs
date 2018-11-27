f26c6a6bd5bc1ea1f84a07b86029aca9da717258
valikneskazhu94 <valik_ne_skazhu@ukr.net>
2018-11-27 20:38:53 +0200

    Bron

---
+++ BOOKING_AUDIENCES/BOOKING_AUDIENCES/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BOOKING_AUDIENCES
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            Label header = new Label
+            {
+                Text = "STEP",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                BackgroundColor = Color.Blue,
+                HorizontalTextAlignment = TextAlignment.Center
+            };
+            string[] phones = new string[] { "iPhone 7", "Samsung Galaxy S8", "Huawei P10", "LG G6" };
+
+            ListView listView = new ListView();
+            
+            listView.ItemsSource = phones;
+            Content = new StackLayout { Children = { header, listView } };
+
+        }
+
+
+    }
+}
